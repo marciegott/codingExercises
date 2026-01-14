4dd593ef2093b97c58729d3be16b9fc94bb9217a
Marcie <marciegottschalk@MacBook-Air-de-Marcie.local>
2026-01-14 15:25:58 +0100

    subindo objetos-facil

---
+++ objetos-facil.js
@@ -0,0 +1,17 @@
+// 1. Crie um objeto chamado objetoVazio que não tenha nenhuma propriedade.
+const objetoVazio = {};
+
+/* 
+2. Crie um objeto chamado pessoa com as propriedades:
+nome: "Seu Nome" (string)
+idade: 30 (number)
+*/
+
+const pessoa = {
+    nome: 'Marcie',
+    idade: 30
+};
+
+console.log(pessoa);
+
+console.log('oi');
